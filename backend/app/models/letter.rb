3ee55209d54e719db5ff7cee0afd7be0befed69b
Ana <anaclimgo@gmail.com>
2026-03-27 14:19:28 -0300

    feat: created letter model with readable_at and deliver_at methods

--- backend/app/models/letter.rb
+++ backend/app/models/letter.rb
@@ -1,3 +1,23 @@
 class Letter < ApplicationRecord
   belongs_to :user
+
+  before_create :set_readable_at
+  validate :deliver_at_must_be_in_future
+
+  validates :deliver_at, presence: true
+
+  private
+
+  def set_readable_at
+    self.readable_at = deliver_at
+  end
+
+  def deliver_at_must_be_in_future
+    return if deliver_at.blank?
+
+    if deliver_at < 3.days.from_now
+      errors.add(:deliver_at, "must be at least 3 days in the future")
+    end
+  end
+
 end
