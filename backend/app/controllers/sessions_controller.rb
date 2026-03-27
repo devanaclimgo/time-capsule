999989024dde23757c097615d5d73eca25068125
Ana <anaclimgo@gmail.com>
2026-03-27 15:20:32 -0300

    refactor: restructure Users module in registrations and sessions controllers

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,24 +1,26 @@
-class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+module Users
+  class Users::SessionsController < Devise::SessionsController
+    respond_to :json
 
-  private
+    private
 
-  def respond_to_on_destroy
-    render json: {
-      message: 'Logged out successfully'
-    }, status: :ok
-  end
-
-  def respond_with(resource, _opts = {})
-    if resource.persisted?
+    def respond_to_on_destroy
       render json: {
-        message: 'Logged in successfully',
-        user: resource
+        message: 'Logged out successfully'
       }, status: :ok
-    else
-      render json: {
-        error: 'Invalid email/username or password'
-      }, status: :unauthorized
+    end
+
+    def respond_with(resource, _opts = {})
+      if resource.persisted?
+        render json: {
+          message: 'Logged in successfully',
+          user: resource
+        }, status: :ok
+      else
+        render json: {
+          error: 'Invalid email/username or password'
+        }, status: :unauthorized
+      end
     end
   end
 end
