575d718bb79221a442e079bbb79f511ccc5b62d1
Ana <anaclimgo@gmail.com>
2026-03-27 13:55:22 -0300

    feat: migrated letter model

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_26_194606) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_27_165426) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "letters", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "deliver_at"
+    t.boolean "delivered"
+    t.datetime "readable_at"
+    t.string "recipient"
+    t.string "sender"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_letters_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +38,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_26_194606) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "letters", "users"
 end
