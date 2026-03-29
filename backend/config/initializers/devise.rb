49b30c9c1831ecf83920fac7f5246aa6ab53a869
Ana <anaclimgo@gmail.com>
2026-03-29 17:30:31 -0300

    fix: update jwt secret configuration to use bang method

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -87,7 +87,7 @@ Devise.setup do |config|
   config.navigational_formats = []
   
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
 
     jwt.dispatch_requests = [
       ['POST', %r{^/users/sign_in$}]
