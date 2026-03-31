effdd68e2d482e450ed58c029830302f86e90edb
Ana <anaclimgo@gmail.com>
2026-03-31 15:42:12 -0300

    fix: removed toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,10 @@ import SignUpPage from "./sign_up/page";
 import LoginPage from "./login/page";
 import DashboardPage from "./dashboard/page";
 import WriteLetterPage from "./write/page";
-import { Toaster } from "sonner";
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Toaster
-        richColors
-        position="top-right"
-        toastOptions={{
-          style: {
-            borderRadius: "12px",
-          },
-        }}
-      />
       <Route path="/" element={<LandingPage />} />
       <Route path="/sign_up" element={<SignUpPage />} />
       <Route path="/login" element={<LoginPage />} />
