9689f1eb4b83a404a049b6f9c093198e5df57196
Ana <anaclimgo@gmail.com>
2026-04-27 19:10:50 -0300

    feat: wrap Sidekiq cron job creation in a conditional block for server configuration

--- backend/config/initializers/sidekiq_schedule.rb
+++ backend/config/initializers/sidekiq_schedule.rb
@@ -1,7 +1,13 @@
-require "sidekiq/cron/job"
+if defined?(Sidekiq) && ENV["REDIS_URL"].present?
+  require "sidekiq/cron/job"
 
-Sidekiq::Cron::Job.create(
-  name: "Deliver letters every 5 minutes",
-  cron: "*/5 * * * *",
-  class: "DeliverLettersJob"
-)
+  Sidekiq.configure_server do |config|
+    config.on(:startup) do
+      Sidekiq::Cron::Job.create(
+        name: "Deliver letters every 5 minutes",
+        cron: "*/5 * * * *",
+        class: "DeliverLettersJob"
+      )
+    end
+  end
+end
