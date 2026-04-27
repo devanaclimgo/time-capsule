7c2f04ae19215a92add51bb6d1fdc7fe93164301
Ana <anaclimgo@gmail.com>
2026-04-27 18:49:37 -0300

    feat: update DeliverLettersJob to send letters asynchronously using deliver_later

--- backend/app/jobs/deliver_letters_job.rb
+++ backend/app/jobs/deliver_letters_job.rb
@@ -5,7 +5,7 @@ class DeliverLettersJob
     letters = Letter.where("deliver_at <= ? AND delivered = ?", Time.current, false)
 
     letters.find_each do |letter|
-      LetterMailer.send_letter(letter)
+      LetterMailer.send_letter(letter).deliver_later
       letter.update!(delivered: true)
     end
   end
